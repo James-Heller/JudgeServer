c1729cd7813587b38844573b01728d8884d23ee3
JamesTang <James-Heller@Outlook.com>
2023-04-07 11:40:56 +0800

    Improve some features

--- src/main/kotlin/pers/jamestang/judgeServer/Main.kt
+++ src/main/kotlin/pers/jamestang/judgeServer/Main.kt
@@ -1,10 +1,24 @@
 package pers.jamestang.judgeServer
 
+import pers.jamestang.judgeServer.config.Config
+import java.io.File
+
 
 fun main() {
 
-
+    checkDirExist(Config.srcBasePath)
+    checkDirExist(Config.executableBasePath)
+    checkDirExist(Config.executeResultBasePath)
+    checkDirExist(Config.logBasePath)
 }
 
+private fun checkDirExist(path: String){
 
+    val dir = File(path)
 
+    if (dir.isDirectory){
+        return
+    }
+
+    dir.mkdir()
+}
